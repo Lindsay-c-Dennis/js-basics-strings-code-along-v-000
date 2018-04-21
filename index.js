dc112c2aec248371ca843af301cba4986b92dea0
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-04-21 18:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = currentUser(0);
+const shortGreeting = `Welcome, ${currentUser(0)}!`;
